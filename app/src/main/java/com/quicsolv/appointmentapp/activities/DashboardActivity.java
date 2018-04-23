14bdb251dc9ce8fed0e149d4556c94598002ca1e
Tushar <tusharpatil.tnp@gmail.com>
2018-04-23 15:34:06 +0530

    Added nav drawer menus with icons

--- app/src/main/java/com/quicsolv/appointmentapp/activities/DashboardActivity.java
+++ app/src/main/java/com/quicsolv/appointmentapp/activities/DashboardActivity.java
@@ -82,17 +82,17 @@ public class DashboardActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_my_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_my_questionnarie) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_my_appointments) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_request_appointment) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_reset_password) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_logout) {
 
         }
 
