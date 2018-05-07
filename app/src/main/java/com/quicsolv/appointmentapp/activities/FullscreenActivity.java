11e2fdce9980d88408cdc09a6ac1b3e8ab504cd2
Tushar <tusharpatil.tnp@gmail.com>
2018-05-07 14:09:56 +0530

    Show progress while item on loading webview

--- app/src/main/java/com/quicsolv/appointmentapp/activities/FullscreenActivity.java
+++ app/src/main/java/com/quicsolv/appointmentapp/activities/FullscreenActivity.java
@@ -5,14 +5,13 @@ import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.quicsolv.appointmentapp.R;
-import com.squareup.picasso.Picasso;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -22,7 +21,8 @@ public class FullscreenActivity extends AppCompatActivity {
 
     private static final String TAG = "FullscreenActivity";
     private Context mContext;
-    private ImageView ivReport, ivBack;
+    private ImageView ivBack;
+    private WebView ivReport;
     private ProgressBar progressBar;
     Matrix matrix = new Matrix();
     Matrix savedMatrix = new Matrix();
@@ -52,7 +52,12 @@ public class FullscreenActivity extends AppCompatActivity {
     }
 
     private void getIds() {
-        ivReport = (ImageView) findViewById(R.id.image_report);
+        ivReport = (WebView) findViewById(R.id.webview_report);
+        ivReport.getSettings().setJavaScriptEnabled(true);
+        ivReport.getSettings().setLoadWithOverviewMode(true);
+        ivReport.getSettings().setUseWideViewPort(true);
+        ivReport.getSettings().setBuiltInZoomControls(true);
+
         ivBack = (ImageView) findViewById(R.id.iv_back);
 
         progressBar = (ProgressBar) findViewById(R.id.progress_report);
@@ -61,8 +66,33 @@ public class FullscreenActivity extends AppCompatActivity {
         if (getIntent() != null && getIntent().getExtras() != null) {
             String imgPath = getIntent().getExtras().getString("ReportImage");
 
-            Picasso.with(mContext).load(imgPath).error(R.id.iv_app_logo).into(ivReport);
-            progressBar.setVisibility(View.GONE);
+            progressBar.setVisibility(View.VISIBLE);
+
+            ivReport.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+                    return true;
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    progressBar.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                    progressBar.setVisibility(View.GONE);
+
+                }
+            });
+            if (imgPath.contains("pdf")) {
+                ivReport.loadUrl("https://docs.google.com/viewer?url=" + imgPath);
+            } else {
+                ivReport.loadUrl(imgPath);
+            }
+
+
         }
 
         ivBack.setOnClickListener(new View.OnClickListener() {
@@ -71,101 +101,5 @@ public class FullscreenActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-        ivReport.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                ImageView view = (ImageView) v;
-                dumpEvent(event);
-
-                // Handle touch events here...
-                switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                    case MotionEvent.ACTION_DOWN:
-                        savedMatrix.set(matrix);
-                        start.set(event.getX(), event.getY());
-                        Log.d(TAG, "mode=DRAG");
-                        mode = DRAG;
-                        break;
-                    case MotionEvent.ACTION_POINTER_DOWN:
-                        oldDist = spacing(event);
-                        Log.d(TAG, "oldDist=" + oldDist);
-                        if (oldDist > 10f) {
-                            savedMatrix.set(matrix);
-                            midPoint(mid, event);
-                            mode = ZOOM;
-                            Log.d(TAG, "mode=ZOOM");
-                        }
-                        break;
-                    case MotionEvent.ACTION_UP:
-                    case MotionEvent.ACTION_POINTER_UP:
-                        mode = NONE;
-                        Log.d(TAG, "mode=NONE");
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        if (mode == DRAG) {
-                            // ...
-                            matrix.set(savedMatrix);
-                            matrix.postTranslate(event.getX() - start.x, event.getY()
-                                    - start.y);
-                        } else if (mode == ZOOM) {
-                            float newDist = spacing(event);
-                            Log.d(TAG, "newDist=" + newDist);
-                            if (newDist > 10f) {
-                                matrix.set(savedMatrix);
-                                float scale = newDist / oldDist;
-                                matrix.postScale(scale, scale, mid.x, mid.y);
-                            }
-                        }
-                        break;
-                }
-
-                view.setImageMatrix(matrix);
-                return true;
-            }
-        });
-    }
-
-    private void dumpEvent(MotionEvent event) {
-        String names[] = {"DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE",
-                "POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?"};
-        StringBuilder sb = new StringBuilder();
-        int action = event.getAction();
-        int actionCode = action & MotionEvent.ACTION_MASK;
-        sb.append("event ACTION_").append(names[actionCode]);
-        if (actionCode == MotionEvent.ACTION_POINTER_DOWN
-                || actionCode == MotionEvent.ACTION_POINTER_UP) {
-            sb.append("(pid ").append(
-                    action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
-            sb.append(")");
-        }
-        sb.append("[");
-        for (int i = 0; i < event.getPointerCount(); i++) {
-            sb.append("#").append(i);
-            sb.append("(pid ").append(event.getPointerId(i));
-            sb.append(")=").append((int) event.getX(i));
-            sb.append(",").append((int) event.getY(i));
-            if (i + 1 < event.getPointerCount())
-                sb.append(";");
-        }
-        sb.append("]");
-        Log.d(TAG, sb.toString());
-    }
-
-    /**
-     * Determine the space between the first two fingers
-     */
-    private float spacing(MotionEvent event) {
-        float x = event.getX(0) - event.getX(1);
-        float y = event.getY(0) - event.getY(1);
-        return (x * x + y * y);
-    }
-
-    /**
-     * Calculate the mid point of the first two fingers
-     */
-    private void midPoint(PointF point, MotionEvent event) {
-        float x = event.getX(0) + event.getX(1);
-        float y = event.getY(0) + event.getY(1);
-        point.set(x / 2, y / 2);
     }
 }
